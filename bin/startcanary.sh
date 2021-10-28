c993d32e44192296ea85fa996eb73f7b9ac7f2c6
Dario B <feddar@users.noreply.github.com>
2021-10-28 14:03:29 +0200

    opencanary & exim

---
+++ bin/startcanary.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#exec tail -f /data/opencanary.log &
+
+OPENCANARY_NODE_ID=${OPENCANARY_NODE_ID:-$(hostname -f)}
+
+sed -i "s/device\.node_id.*/device\.node_id\": \"$OPENCANARY_NODE_ID\",/" /root/.opencanary.conf
+
+source /opt/opencanary/virtualenv/bin/activate && opencanaryd --start
+
