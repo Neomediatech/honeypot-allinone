2636403ccc54f4dfee4b3ab6a8691768cfe9ddf1
Dario B <feddar@users.noreply.github.com>
2021-11-29 21:02:15 +0100

    mistake

--- conf/nginx/html/index.php
+++ conf/nginx/html/index.php
@@ -116,7 +116,7 @@ if ($key == '') { ?>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2018-∞</p>
     </form>
-<?php { ?>
+<?php } ?>
   </body>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
