e21ea99c91f734d0f973e7981bb6a55e8a309227
Dario B <feddar@users.noreply.github.com>
2021-11-30 20:19:30 +0100

    changed bin/entrypoint-cron.sh

--- bin/entrypoint-cron.sh
+++ bin/entrypoint-cron.sh
@@ -6,16 +6,12 @@ TMPDIR="/var/cache/cron"
 mkdir -p "$TMPDIR"
 
 # if cron file exists calculate MD5 for it and put in "orig-cron.md5" file, then execute crontab
-if [ -f "$SRC_CRON" ]; then
-  md5sum "$SRC_CRON" > "$TMPDIR"/orig-cron.md5
-  if [ $? -ne 0 ]; then
-    crontab "$SRC_CRON"
-  fi
-else
+if [ ! -f "$SRC_CRON" ]; then
   mkdir -p "$SRC_CRON"
   echo "#" >> "$SRC_CRON"
-  md5sum "$SRC_CRON" > "$TMPDIR"/orig-cron.md5
 fi
+md5sum "$SRC_CRON" > "$TMPDIR"/orig-cron.md5
+crontab "$SRC_CRON"
 
 # run this script to check new crontabs
 /srv/scripts/check-cron.sh &
